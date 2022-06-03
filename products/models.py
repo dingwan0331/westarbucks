ff3988e91945cdc050a0dbb2f488ecd01e68459b
dingwan <dingwan0331@gmail.com>
2022-06-03 22:37:44 +0900

    CREATE: 1차 models.py작성

---
+++ products/models.py
@@ -0,0 +1,67 @@
+from pydoc import describe
+from unicodedata import category
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'menus'
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'categories'
+
+class Drink(models.Model):
+    name = models.CharField(max_length=45)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    korean_name = models.CharField(max_length=45)
+    english_name = models.CharField(max_length=45)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'drinks'
+
+class Allergy_drink(models.Model):
+    allergy = models.ForeignKey('Allergy', on_delete=models.CASCADE)
+    drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'allergy_drink'
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=45)
+    
+    class Mete:
+        db_table = 'allergies'
+
+class Nutrition:
+    one_serving_kcal = models.DecimalField(max_digits=10,decimal_places=2 )
+    sodium_mg = models.DecimalField(max_digits=10,decimal_places=2 )
+    saturated_fat_g = models.DecimalField(max_digits=10,decimal_places=2 )
+    sugars_g = models.DecimalField(max_digits=10,decimal_places=2 )
+    protein_g = models.DecimalField(max_digits=10,decimal_places=2 )
+    caffeine_mg = models.DecimalField(max_digits=10,decimal_places=2 )
+    size = models.ForeignKey('Size',on_delete=models.CASCADE)
+    drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'nutritions'
+
+class Size:
+    name = models.CharField(max_length=45)
+    size_ml = models.CharField(max_length=45)
+    size_fluid_ounce = models.CharField(max_length=45)
+
+    class Mete:
+        db_table = 'sizes'
+
+class Image:
+    drink = models.ForeignKey('Drink',on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=2000)
+    
+    class Meta:
+        db_table = 'image_urls'
